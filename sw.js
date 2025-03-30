5ecf669a64f1fd8eb9b47362c3da4a55a04f6143
inducedcandle172 <inducedcandle172@gmail.com>
2025-03-30 20:45:54 +0800

    Update: Optimize for Netlify deployment and iOS support

--- sw.js
+++ sw.js
@@ -16,19 +16,62 @@ const urlsToCache = [
     '/duration.html',
     '/budget.html',
     '/itinerary.html',
-    '/icons/icon-192x192.png'
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
+// Install service worker
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
+// Cache and return requests
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(response => {
+                // Return cached version or fetch new
+                return response || fetch(event.request)
+                    .then(response => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
+            })
+    );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 }); 

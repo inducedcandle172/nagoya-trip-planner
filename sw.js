e2f0667c81825a493d3ad62b56f1a3aa37e5bf1a
inducedcandle172 <inducedcandle172@gmail.com>
2025-03-30 19:34:27 +0800

    Add PWA support and iOS Add to Home Screen compatibility

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
-const CACHE_NAME = 'nagoya-trip-planner-v1';
+const CACHE_NAME = 'nagoya-trip-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/styles.css',
     '/manifest.json',
-    '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png',
+    '/translations.js',
     '/day1.html',
     '/day2.html',
     '/day3.html',
@@ -19,20 +12,19 @@ const urlsToCache = [
     '/day5.html',
     '/day6.html',
     '/day7.html',
+    '/travelers.html',
+    '/duration.html',
     '/budget.html',
     '/itinerary.html',
-    'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ];
 
 // Install service worker
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
@@ -41,22 +33,19 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Return cached version or fetch new
-                return response || fetch(event.request)
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
                     .then(response => {
-                        // Check if we received a valid response
                         if (!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
                         }
-
-                        // Clone the response
                         const responseToCache = response.clone();
-
                         caches.open(CACHE_NAME)
                             .then(cache => {
                                 cache.put(event.request, responseToCache);
                             });
-
                         return response;
                     });
             })

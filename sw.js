0d4e0d66a24ccc640d3fd1d1ecb0d165da4ed24e
inducedcandle172 <inducedcandle172@gmail.com>
2025-03-30 20:00:30 +0800

    Refactor: Simplify manifest and service worker for better iOS compatibility

--- sw.js
+++ sw.js
@@ -16,120 +16,22 @@ const urlsToCache = [
     '/duration.html',
     '/budget.html',
     '/itinerary.html',
-    '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
     '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
     '/icons/icon-512x512.png',
-    // iPad Splash Screens
-    '/icons/splash-1536x2048.png',
-    '/icons/splash-1668x2388.png',
-    '/icons/splash-1668x2224.png',
-    '/icons/splash-1620x2160.png',
-    // iPhone Splash Screens
-    '/icons/splash-1290x2796.png',
-    '/icons/splash-1179x2556.png',
-    '/icons/splash-1284x2778.png',
-    '/icons/splash-1170x2532.png',
-    '/icons/splash-1125x2436.png',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// Install service worker
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Cache and return requests
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                // Return cached version or fetch new
-                return response || fetch(event.request)
-                    .then(response => {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // Clone the response
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    });
-            })
+            .then(response => response || fetch(event.request))
     );
-});
-
-// Update service worker
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Push Notification
-self.addEventListener('push', event => {
-    const options = {
-        body: event.data.text(),
-        icon: '/icons/icon-192x192.png',
-        badge: '/icons/badge.png',
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-        },
-        actions: [
-            {
-                action: 'explore',
-                title: 'View Itinerary',
-                icon: '/icons/checkmark.png'
-            },
-            {
-                action: 'close',
-                title: 'Close',
-                icon: '/icons/xmark.png'
-            }
-        ]
-    };
-
-    event.waitUntil(
-        self.registration.showNotification('Nagoya Trip Planner', options)
-    );
-});
-
-// Notification Click
-self.addEventListener('notificationclick', event => {
-    event.notification.close();
-
-    if (event.action === 'explore') {
-        event.waitUntil(
-            clients.openWindow('/')
-        );
-    }
 }); 
